2ee41d1160e53056785e2b150386ae17a356a00d
Bruno Leal <leal.bruno997@gmail.com>
2020-01-10 21:15:12 +0000

    Test edition pages made

--- contest/views.py
+++ contest/views.py
@@ -582,8 +582,6 @@ def admin_choose_test(request, id):
 
 		# print_variables_debug(["test id:", test_id])
 
-	context.update({'form': form})
-
 	return render(request, template_name, context)
 
 
@@ -704,78 +702,58 @@ def admin_test_editor(request, id, t_id):
 
 	# print_variables_debug(["Test:", test])
 
-	context = {'contest': contest_obj}
-	context.update({'test': test})
+	context = {'test': test}
 
-	teams = Team.objects.filter(contest__id=id)  # get all teams associated with this contest
-
-	for t in teams:
-		t.members = TeamMember.objects.filter(team=t)
-
-	context.update({'teams': teams})
-
-	# TODO Make it better
-	query = "Select ca.id, c.name as team_name, ca.grade as team_grade, maxs.atempts as team_atempts" \
-			"	from (" \
-			"		select max(id) as id, count(id) as atempts, team_id" \
-			"			from contest_atempt" \
-			"				where contest_id = " + str(contest_obj.id) + \
-			"					group by team_id" \
-			"	) maxs" \
-			"		inner join contest_atempt ca on ca.id = maxs.id " \
-			"		join contest_team c on ca.team_id = c.id" \
-			"		join auth_user au on ca.user_id = au.id"
-
-	grades = Atempt.objects.raw(query)
-
-	context.update({'grades': grades})
-
-	# TODO Make it better
-	query = "SELECT ca.id, maxs.team_atempts, maxs.team_id, au.username as username, au.first_name, au.last_name," \
-			"umax.atempts as user_atempts" \
-			"	FROM (" \
-			"		select max(id) as id, count(id) as team_atempts, team_id" \
-			"			from contest_atempt" \
-			"				where contest_id = " + str(contest_obj.id) + \
-			"					group by team_id" \
-			"	) maxs" \
-			"		inner join contest_atempt ca on ca.id = maxs.id" \
-			"		join contest_teammember ct on ct.team_id = ca.team_id" \
-			"		join contest_team c on ca.team_id = c.id" \
-			"		join auth_user au on au.id = ct.user_id" \
-			"		join (" \
-			"			select max(id) as id, count(id) as atempts, user_id" \
-			"				from contest_atempt" \
-			"					where contest_id = 1" \
-			"						group by user_id" \
-			"		) umax on umax.user_id = ct.user_id" \
-			"			order by atempts asc"
-
-	atempts = Atempt.objects.raw(query)
-
-	context.update({'atempts': atempts})
-
-	form = TeamMemberForm(request.POST or None)
+	form = TestForm(request.POST or None)
 
 	if form.is_valid():
-		t_id = form.cleaned_data.get("team_id")
-		# verificar codigo team join e team status
-		return redirect(os.path.join(contest_obj.get_absolute_url(), 'admin-view/team/' + str(t_id) + '/status/'))
 
-	context.update({'form': form})
-	#
-	# bug = ["INFO IN THE HTML:", "***************TEAMS***********"]
-	#
-	# for t in teams:
-	#     es = ""
-	#     for e in TeamMember.objects.filter(team=t):
-	#         if es == "":
-	#             es = str(e.user.first_name) + " " + str(e.user.last_name)
-	#         else:
-	#             es += "; " + str(e.user.first_name) + " " + str(e.user.last_name)
-	#     bug.append(str(t) + " - " + str(es))
-	#
-	# print_variables_debug(bug)
+		print_variables_debug(['\n\noverride value:\n\n', request.POST.getlist('override'),
+							   '\n\ncpu value:\n\n', request.POST.getlist('cpu'),
+							   '\n\nmem value:\n\n', request.POST.getlist('mem'),
+							   '\n\nspace value:\n\n', request.POST.getlist('space'),
+							   '\n\ncore value:\n\n', request.POST.getlist('core'),
+							   '\n\nnproc value:\n\n', request.POST.getlist('nproc'),
+							   '\n\nfsize value:\n\n', request.POST.getlist('fsize'),
+							   '\n\nstack value:\n\n', request.POST.getlist('stack'),
+							   '\n\nclock value:\n\n', request.POST.getlist('clock')])
+
+		if 'on' in request.POST.getlist('override'):
+			test_override = True
+		else:
+			test_override = False
+		test_cpu = request.POST.getlist('cpu')[0]
+		test_mem = request.POST.getlist('mem')[0]
+		test_space = request.POST.getlist('space')[0]
+		test_core = request.POST.getlist('core')[0]
+		test_nproc = request.POST.getlist('nproc')[0]
+		test_fsize = request.POST.getlist('fsize')[0]
+		test_stack = request.POST.getlist('stack')[0]
+		test_clock = request.POST.getlist('clock')[0]
+
+		print_variables_debug(["Input\n", test_override, test_cpu, test_mem, test_space, test_core, test_nproc,
+							   test_fsize, test_stack, test_clock])
+
+		test = Test.objects.get(contest_id=id, id=t_id)
+
+		print_variables_debug(["Before\n", test.override_exec_options, test.cpu, test.mem, test.space, test.core,
+							   test.nproc, test.fsize, test.stack, test.clock])
+
+		test.override_exec_options = test_override
+		test.cpu = test_cpu
+		test.mem = test_mem
+		test.space = test_space
+		test.core = test_core
+		test.nproc = test_nproc
+		test.fsize = test_fsize
+		test.stack = test_stack
+		test.clock = test_clock
+		test.save()
+
+		print_variables_debug(["After\n", test.override_exec_options, test.cpu, test.mem, test.space, test.core,
+							   test.nproc, test.fsize, test.stack, test.clock])
+
+		return redirect(os.path.join(contest_obj.get_absolute_url(), 'admin-view/test/chooser/'))
 
 	return render(request, template_name, context)
 
