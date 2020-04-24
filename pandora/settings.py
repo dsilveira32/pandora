8f377ed9eb843f95dc0c9b4c666288d6d9d12a63
Pedro Arroz <pedro.serra@ulusofona.pt>
2020-04-24 16:46:30 +0100

    fix test edit menu

--- pandora/settings.py
+++ pandora/settings.py
@@ -104,18 +104,7 @@ LOGIN_REDIRECT_URL = 'home'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.mysql',
- #        'NAME': 'pandora',
-  #       'USER' : DB_USER,
- #        'PASSWORD' : DB_PASSWORD,
- #        'HOST' : 'localhost',
- #        'PORT' : '3306'
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
